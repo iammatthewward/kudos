b71aae28b1bfff8c01aabb4e6329a1ed1929e3e0
Matt Ward <iammatthewward@gmail.com>
2019-10-03 22:36:03 +0100

    feat(plug): add content_type plug

---
+++ lib/plug/content_type.ex
@@ -0,0 +1,14 @@
+defmodule Kudos.Plug.ContentType do
+  import Plug.Conn
+
+  def init(options), do: options
+
+  def call(conn, opts) do
+    if :content_type in Keyword.keys(opts) do
+      conn |> put_resp_content_type(opts[:content_type])
+    else
+      conn
+    end
+  end
+
+end
---
+++ test/plug/content_type_test.exs
@@ -0,0 +1,28 @@
+defmodule ContentTypeTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias Kudos.Plug.ContentType
+
+  @opts ContentType.init(content_type: "application/json")
+  @no_opts ContentType.init([])
+
+  test "sets the given content type" do
+    conn =
+      :get
+      |> conn("/", "")
+      |> ContentType.call(@opts)
+
+    assert {"content-type", "application/json; charset=utf-8"} in conn.resp_headers
+  end
+
+  test "doesn't set the content type if missing from opts" do
+    conn =
+      :get
+      |> conn("/", "")
+      |> ContentType.call(@no_opts)
+
+    refute {"content-type", "application/json; charset=utf-8"} in conn.resp_headers
+  end
+
+end
