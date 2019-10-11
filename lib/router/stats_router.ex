8668fc00bdd65017bfa303d58c059e9b92ca664e
Matt Ward <iammatthewward@gmail.com>
2019-10-11 20:13:37 +0100

    feat(stats): add get all stats endpoint

--- lib/router/stats_router.ex
+++ lib/router/stats_router.ex
@@ -1,18 +1,21 @@
 defmodule Kudos.StatsRouter do
   use Plug.Router
+  alias Kudos.Praise
+  alias Kudos.Repo
 
-  plug :match
-  plug :dispatch
+  plug(:match)
+  plug(:dispatch)
 
   get "/" do
-    conn = put_status(conn, 200)
+    all_results = Praise |> Repo.all()
 
-    render_json(conn, %{})
+    conn
+    |> put_status(200)
+    |> render_json(all_results)
   end
 
   def render_json(%{status: status} = conn, data) do
     body = Jason.encode!(data)
-    send_resp(conn, (status || 200), body)
+    send_resp(conn, status || 200, body)
   end
-
 end
--- lib/schema/praise.ex
+++ lib/schema/praise.ex
@@ -2,8 +2,9 @@ defmodule Kudos.Praise do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @derive {Jason.Encoder, only: [:id, :message]}
   schema "praise" do
-    field :message, :string
+    field(:message, :string)
   end
 
   def changeset(praise, params \\ %{}) do
